392e2146270cf6d802fca8de587f49fd86131a61
Muhammad Usman <osmanjajja@gmail.com>
2021-07-03 23:45:39 +0500

    Adding the Put(Update) Route

--- index.js
+++ index.js
@@ -20,6 +20,30 @@ app.get("/api/generas", (req, res) => {
   res.send(generas);
 });
 
+// Update Request
+
+app.put("/api/generas/:id", (req, res) => {
+  // Find the generas with given id.
+  const gener = generas.find((g) => g.id === parseInt(req.params.id));
+  if (!gener) {
+    return res.status(404).send("Course with given id is not found");
+  }
+  // validate the generas.
+  const schema = Joi.object({
+    name: Joi.string().min(3).max(30).required(),
+  });
+  const { error } = schema.validate(req.body);
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
+  // if true update the genears and redirect the user to course page.
+  else {
+    gener.name = req.body.name;
+    res.send(gener);
+    return;
+  }
+});
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
