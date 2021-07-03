f40f27d7f79aa6f42d1d26d872cdf0a7ce5d9e48
Muhammad Usman <osmanjajja@gmail.com>
2021-07-04 00:30:24 +0500

    Adding the Post Route

--- index.js
+++ index.js
@@ -19,6 +19,31 @@ app.get("/", (req, res) => {
 app.get("/api/generas", (req, res) => {
   res.send(generas);
 });
+// Create Route
+
+app.post("/api/generas", (req, res) => {
+  // Validate the gener name.
+
+  const schema = Joi.object({
+    name: Joi.string().min(3).max(30).required(),
+  });
+  const { error, value } = schema.validate(req.body);
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
+  // if true update the genears and redirect the user to course page.
+  if (value) {
+    const gener = {
+      id: generas.length + 1,
+      name: req.body.name,
+    };
+    generas.push(gener);
+    return res.send(gener);
+  }
+
+  // add to the gener list.
+  // redirect user to newly created gener.
+});
 
 // Update Request
 
@@ -39,8 +64,18 @@ app.put("/api/generas/:id", (req, res) => {
   // if true update the genears and redirect the user to course page.
   else {
     gener.name = req.body.name;
-    res.send(gener);
-    return;
+    return res.send(gener);
+  }
+});
+// Delete route
+app.delete("/api/generas/:id", (req, res) => {
+  const gener = generas.find((g) => g.id === parseInt(req.params.id));
+  if (!gener) {
+    return res.status(404).send("Course with given id is not found");
+  } else {
+    const index = generas.indexOf(gener);
+    generas.splice(index);
+    return res.send(gener);
   }
 });
 
